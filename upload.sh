7573c2df6a8110e967da758817ec3caa345130cd
Fisher Yu <i@yf.io>
2018-11-13 16:27:21 -0800

    simple travis upload config

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync _build/html s3://www.scalabel.ai/doc --exclude ".DS_Store" --acl public-read
-aws s3 sync instructions s3://www.scalabel.ai/doc/instructions --exclude ".DS_Store" --acl public-read
-aws s3 sync demo s3://www.scalabel.ai/doc/demo --exclude ".DS_Store" --acl public-read
+./build.sh
+aws s3 sync _doc s3://www.scalabel.ai/doc --exclude ".DS_Store" --acl public-read
